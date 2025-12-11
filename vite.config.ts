21eb2e6b76b6fffbadfc12f95bfbf9b3fa4ca9fa
Lukas <lukasschill@icloud.com>
2025-12-11 09:38:23 -0500

    update vite config for CF dev tunnels

--- vite.config.ts
+++ vite.config.ts
@@ -1,40 +1,46 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import devtoolsJson from 'vite-plugin-devtools-json';
 
-export default defineConfig({
-	plugins: [tailwindcss(), sveltekit(), devtoolsJson()],
-	test: {
-		expect: { requireAssertions: true },
-		projects: [
-			{
-				extends: './vite.config.ts',
-				test: {
-					name: 'client',
-					environment: 'browser',
-					browser: {
-						enabled: true,
-						headless: false,
-						provider: 'playwright',
-						instances: [{ browser: 'chromium' }]
-					},
-					includeTaskLocation: true,
-					include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
-					exclude: ['src/lib/server/**'],
-					setupFiles: ['./vitest-setup-client.ts']
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), '');
+	return {
+		plugins: [tailwindcss(), sveltekit(), devtoolsJson()],
+		server: {
+			allowedHosts: env.CF_TUNNEL_URL ? [env.CF_TUNNEL_URL] : []
+		},
+		test: {
+			expect: { requireAssertions: true },
+			projects: [
+				{
+					extends: './vite.config.ts',
+					test: {
+						name: 'client',
+						environment: 'browser',
+						browser: {
+							enabled: true,
+							headless: false,
+							provider: 'playwright',
+							instances: [{ browser: 'chromium' }]
+						},
+						includeTaskLocation: true,
+						include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
+						exclude: ['src/lib/server/**'],
+						setupFiles: ['./vitest-setup-client.ts']
+					}
+				},
+				{
+					extends: './vite.config.ts',
+					test: {
+						name: 'server',
+						environment: 'node',
+						includeTaskLocation: true,
+						include: ['src/**/*.{test,spec}.{js,ts}'],
+						exclude: ['src/**/*.svelte.{test,spec}.{js,ts}']
+					}
 				}
-			},
-			{
-				extends: './vite.config.ts',
-				test: {
-					name: 'server',
-					environment: 'node',
-					includeTaskLocation: true,
-					include: ['src/**/*.{test,spec}.{js,ts}'],
-					exclude: ['src/**/*.svelte.{test,spec}.{js,ts}']
-				}
-			}
-		]
-	}
+			]
+		}
+	};
 });
