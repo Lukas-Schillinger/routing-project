359d7816e1384489672b1e71012efd1206e71d3b
Lukas <lukasschill@icloud.com>
2026-01-06 10:44:43 -0500

    fix auth layout

---
+++ src/routes/(app)/auth/register/+layout@.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { AuthLayout } from '$lib/components/auth';
+	import type { LayoutData } from '../$types';
+
+	let { data, children }: { data: LayoutData; children: any } = $props();
+</script>
+
+<AuthLayout>
+	{@render children?.()}
+</AuthLayout>
